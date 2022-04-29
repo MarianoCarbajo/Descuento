5f2aea2d34ce5f75109895ed61489f172695805c
Mariano Carbajo <104107401+cobacho666@users.noreply.github.com>
2022-04-29 14:43:54 -0300

    Initial commit

---
+++ src/descuento.ts
@@ -0,0 +1,5 @@
+let precio: number = 450.5;
+let porcentaje: number = 0.1;
+let descuento: number = precio * porcentaje;
+//let resultado: number = precio - descuento;
+console.log("precio final es: ", precio - descuento);
